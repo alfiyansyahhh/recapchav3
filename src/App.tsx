a84886bcc8d8bae67ed687bc6698f657e671d24e
ilham_alfiyansyah <muhamadilhamalfiyansyah@gmail.com>
2024-09-30 15:47:06 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import Login from './login';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
